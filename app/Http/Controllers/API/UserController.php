fbaf93e827163bb092eeb44de5bb35c3058e5032
dangcaoanh <dangcaoanh1998@gmail.com>
2025-04-03 12:59:01 +0700

    fix use link

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController;
 use Illuminate\Http\Request;
@@ -9,14 +9,14 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends BaseController
-{   
+{
     public function isAuth($id): bool
     {
         return Auth::user()->role == "admin" || Auth::user()->id == $id;
     }
 
     public function index(): JsonResponse
-    {    
+    {
         $users = User::paginate(10);
         return response()->json(['users' => $users]);
     }
@@ -28,11 +28,11 @@ class UserController extends BaseController
     }
 
     public function edit($id): JsonResponse
-    {   
+    {
         if (!$this->isAuth($id)) {
             return response()->json(['error' => 'Bạn không thể chỉnh sửa người dùng này.'], 403);
         }
-        
+
         $user = User::findOrFail($id);
         return response()->json(['user' => $user]);
     }
@@ -43,18 +43,18 @@ class UserController extends BaseController
             'username' => 'required|string|min:3|max:8',
             'email' => 'required|email|unique:users,email,' . $id,
             'description' => 'nullable|string',
-            'photo' => 'nullable|mimes:jpeg,png,jpg,gif|max:2048' 
+            'photo' => 'nullable|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
 
         $user = User::findOrFail($id);
 
         if ($request->hasFile('photo')) {
-            $imageName = time().'.'.$request->photo->extension();  
+            $imageName = time().'.'.$request->photo->extension();
             $request->photo->move(public_path('images'), $imageName);
             $photoUrl = 'images/' . $imageName;
             $user->photo = $photoUrl;
         }
-    
+
         $user->update([
             'username' => $request->username,
             'email' => $request->email,
@@ -65,14 +65,14 @@ class UserController extends BaseController
     }
 
     public function destroy(Request $request, $id): JsonResponse
-    {   
+    {
         if (!$this->isAuth($id)) {
             return response()->json(['error' => 'Bạn không thể xóa người dùng này.'], 403);
         }
-        
+
         $user = User::findOrFail($id);
         $user->delete();
-        
+
         return response()->json(['message' => 'Xóa thành công.']);
     }
 }
